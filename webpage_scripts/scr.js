c7f09115fc012227469ef2966ac6db3b5dd83fc2
Varabe <zertarx@gmail.com>
2019-09-23 23:31:27 -0400

    [add] short answer question clicker

--- webpage_scripts/scr.js
+++ webpage_scripts/scr.js
@@ -3,7 +3,6 @@ function clicker() {
     const list_observer_config = {childList: true};
     const step_observer_config = {attributes: true}
     function main() {
-        
         anims = document.getElementsByClassName("animation-controls")
         for (a of anims) {
             a.children[1].children[0].children[0].checked = true
@@ -15,7 +14,12 @@ function clicker() {
             new MutationObserver(multipleChoiceCallback).observe(el, {childList: true})
             root.children[0].children[1].children[0].children[0].click()
         }
-
+        for (root of document.getElementsByClassName("short-answer-question")) {
+            show_answer_btn = root.getElementsByClassName("show-answer-button")[0]
+            alert = root.getElementsByClassName("explanation")[0]
+            new MutationObserver(shortAnswerCallback).observe(alert, {attributes: true})
+            show_answer_btn.click(); show_answer_btn.click();
+        }
     }
     function listMutationCallback(mutationsList, observer) {
         
@@ -59,6 +63,19 @@ function clicker() {
                     console.log("The answer was correct. Disconnecting.")
                     obs.disconnect()
                 }
+            }
+        }
+    }
+    function shortAnswerCallback(lst, obs) {
+        for (m of lst) {
+            alert = m.target
+            if (alert.classList.contains("forfeit")) {
+                answer = alert.getElementsByClassName("forfeit-answer")[0].innerText
+                obs.disconnect()
+                console.log("Found the right answer. Disconnecting")
+                root = alert.parentElement
+                text_area = root.getElementsByClassName("zb-text-area")[0]
+                text_area.value = answer.trim()
             }
         }
     }
