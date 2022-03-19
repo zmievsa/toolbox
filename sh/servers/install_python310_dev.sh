8a2fdd777fa0283f3e45bd976fcaadac92ef722a
Stanislav Zmiev <szmiev2000@gmail.com>
2022-03-19 17:31:45 -0400

    Add appending python/bin to PATH

--- sh/servers/install_python310_dev.sh
+++ sh/servers/install_python310_dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 set -e
 sudo apt install wget build-essential libreadline-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
 
@@ -10,3 +11,5 @@ cd Python-$1
 ./configure --prefix=/opt/python/$1 --enable-shared --enable-ipv6 --enable-optimizations --with-ensurepip=upgrade LDFLAGS=-Wl,-rpath=/opt/python/$1/lib,--disable-new-dtags
 make
 sudo make install
+read -p "Would you like to add /opt/python/$1/bin/ to PATH? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 0
+echo "PATH=\$PATH:/opt/python/$1/bin/" >> ~/.bashrc
